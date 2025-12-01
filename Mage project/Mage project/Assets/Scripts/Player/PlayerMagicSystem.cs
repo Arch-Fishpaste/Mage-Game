98705d4e5487aca228198c11e30475c9d1ae9c86
Fishpaste09 <leo.knights.886@accesscreative.ac.uk>
2025-12-01 10:14:39 +0000

    RE- adding magic spells for new character controller

---
+++ Mage project/Mage project/Assets/Scripts/Player/PlayerMagicSystem.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PlayerMagicSystem : MonoBehaviour
+{
+    [SerializeField] private float maxMana = 100f;
+    [SerializeField] private float currentMana;
+    [SerializeField] private float manaRechargeRate = 10f;
+    [SerializeField] private float timeBetweenCasts = 0.25f;
+    private float currentCastTimer;
+
+    [SerializeField] private Transform castPoint;
+
+    private PlayerControlls playerControlls;
+
+    private bool castingmagic = false;
+
+
+    void Awake()
+    {
+        playerControlls = new PlayerControlls();
+
+    }
+
+    private void OnEnable()
+    {
+        playerControlls.Enable();
+    }
+
+
+
+    private void OnDisable()
+    {
+        playerControlls.Disable();
+    }
+
+
+
+    private void Update()
+    {
+        bool isSpellcastHeldDown = playerControlls.Controls.Spellcast.ReadValue<float>() > 0f;
+
+        if (!castingmagic && isSpellcastHeldDown)
+        {
+            castingmagic = true;
+            currentCastTimer = 0f;
+            Castspell();
+            print("Casting Magic!");
+        }
+
+        if (castingmagic)
+        {
+            currentCastTimer += Time.deltaTime;
+
+        }
+
+        if (currentCastTimer > timeBetweenCasts) castingmagic = false;
+    }
+
+    void Castspell()
+    {
+        // Implement spell casting logic here
+    }
+}
